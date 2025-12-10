e98161fc71681dd13b50698b09a55cd091a85bde
SAIDURGA123-hub <godavarthisaidurga@gmail.com>
2025-12-10 11:36:02 +0530

    Add files via upload

---
+++ arrays and pointers.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	char str[6]="Hello";
+	char *ptr=str;
+	printf("%c",*ptr);
+	printf("%c",*(ptr+1));
+	printf("%c",*(ptr+2));
+	printf("%c",*(ptr+3));
+	printf("%c",*(ptr+4));
+	return 0;
+}
